f95f30a5f005ac73d0f95abd5d36e6dccf004f2a
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-09-18 21:53:13 +0800

    add animation using js

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ document.getElementById("form").addEventListener("submit", function (event) {
   event.preventDefault();
   alert("Messages has submited <3");
 });
+
+const txtElement = ["Productzilla"];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = "";
+let words = "";
+
+(function ngetik() {
+  if (count == txtElement.length) {
+    count = 0;
+  }
+
+  currentTxt = txtElement[count];
+
+  words = currentTxt.slice(0, ++txtIndex);
+  document.querySelector(".efek-ngetik").textContent = words;
+
+  if (words.length == currentTxt.length) {
+    count++;
+    txtIndex = 0;
+  }
+
+  setTimeout(ngetik, 250);
+})();
